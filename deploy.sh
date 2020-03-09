af12c84f034a16d3c870238407dbcbd07ff1c2f9
odin <odinuv@gmail.com>
2020-03-09 19:31:03 +0100

    initialize build

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,8 @@
 #!/bin/bash
 set -e
 
-# Obtain the component repository and log in
-docker pull quay.io/keboola/developer-portal-cli-v2:latest
-export REPOSITORY=`docker run --rm  \
-    -e KBC_DEVELOPERPORTAL_USERNAME \
-    -e KBC_DEVELOPERPORTAL_PASSWORD \
-    quay.io/keboola/developer-portal-cli-v2:latest \
-    ecr:get-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP}`
-
-eval $(docker run --rm \
-    -e KBC_DEVELOPERPORTAL_USERNAME \
-    -e KBC_DEVELOPERPORTAL_PASSWORD \
-    quay.io/keboola/developer-portal-cli-v2:latest \
-    ecr:get-login ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP})
+# Log in to the repostiory
+eval $(docker run --rm -e AWS_ACCESS_KEY_ID=$DEPLOY_AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$DEPLOY_AWS_ACCESS_KEY_SECRET keboola/aws-cli:latest ecr get-login --region us-east-1 --no-include-email)
 
 # Push to the repository
 docker tag ${APP_IMAGE}:latest ${REPOSITORY}:${TRAVIS_TAG}
