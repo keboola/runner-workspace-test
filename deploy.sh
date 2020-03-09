1849f8b7c4183399d71b4d098c30687522092367
odin <odinuv@gmail.com>
2020-03-09 19:44:35 +0100

    fix: repo name

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Log in to the repostiory
-eval $(docker run --rm -e AWS_ACCESS_KEY_ID=$DEPLOY_AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$DEPLOY_AWS_ACCESS_KEY_SECRET keboola/aws-cli:latest ecr get-login --region us-east-1 --no-include-email)
+eval $(docker run --rm -e AWS_ACCESS_KEY_ID=$DEPLOY_AWS_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$DEPLOY_AWS_ACCESS_KEY_SECRET quay.io/keboola/aws-cli:latest ecr get-login --region us-east-1 --no-include-email)
 
 # Push to the repository
 docker tag ${APP_IMAGE}:latest ${REPOSITORY}:${TRAVIS_TAG}
