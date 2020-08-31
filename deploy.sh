991c184a4c60b42effb006f774e67668f7a1a1c8
odin <odinuv@gmail.com>
2020-08-31 18:48:41 +0200

    chore: deploy to secondary component

--- deploy.sh
+++ deploy.sh
@@ -21,3 +21,15 @@ then
 else
     echo "Skipping deployment to KBC, tag ${TRAVIS_TAG} is not allowed."
 fi
+
+# Update the tag in Keboola Developer Portal -> Deploy to KBC
+if echo ${TRAVIS_TAG} | grep -c '^v\?[0-9]\+\.[0-9]\+\.[0-9]\+$'
+then
+    docker run --rm \
+        -e KBC_DEVELOPERPORTAL_USERNAME \
+        -e KBC_DEVELOPERPORTAL_PASSWORD \
+        quay.io/keboola/developer-portal-cli-v2:latest \
+        update-app-repository ${KBC_DEVELOPERPORTAL_VENDOR} ${KBC_DEVELOPERPORTAL_APP_SYNAPSE} ${TRAVIS_TAG} ecr ${REPOSITORY}
+else
+    echo "Skipping deployment to KBC, tag ${TRAVIS_TAG} is not allowed."
+fi
